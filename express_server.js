c2807e24e03f6283326d6aa49f4ffd7f105f86bc
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-03 20:07:14 -0700

    not working code but mass change

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+//
+// configuration of cookie-parser middleware
+//
+const cookieParser = require('cookie-parser');
 
 //
 // configuration of express app - middleware
@@ -11,9 +15,14 @@ const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
 
 //
-// configuration of cookie-parser middleware
+// POST requests are sent as a Buffer (great for transmitting data but is not readable without this - this is middleware)
 //
-const cookieParser = require('cookie-parser');
+app.use(express.urlencoded({ extended: true }));
+
+//
+//Initiate cookieParser
+//
+app.use(cookieParser());
 
 //
 //Database
@@ -35,18 +44,32 @@ function generateRandomString() {
     return result;
 };
 
-//
-// POST requests are sent as a Buffer (great for transmitting data but is not readable without this - this is middleware)
-//
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser());
 
 app.post('/urls', (req, res) => {
   let uniqueURL = generateRandomString(); // call generate random string for unique url
   const longURL = req.body.longURL
   urlDatabase[uniqueURL] = longURL; // update urlDatabase with new unique url and long url 
   res.redirect(`/urls/${uniqueURL}`); // redirect after submittal to uniqueURL
+});
+
+//
+// Edit longURL
+//
+app.post('/urls/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedLongURL = req.body.updatedLongURL;
+  urlDatabase[id] = updatedLongURL;
+  res.redirect('/urls');
+});
+
+
+//
+// Delete url entries from the database
+//
+app.post('/urls/:id/delete', (req, res) => {
+  const idToDelete = req.params.id;
+  delete urlDatabase[idToDelete]; 
+  res.redirect('/urls');
 });
 
 // logs the POST request body to the console and responds 
@@ -58,24 +81,6 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show.ejs', templateVars)
 });
 
-//
-// Edit longURL
-//
-app.post('/urls/:id', (req, res) => {
-  const id = req.params.id;
-  const updatedLongURL = req.body.updatedLongURL;
-  urlDatabase[id] = updatedLongURL;
-  res.redirect('/urls');
-})
-
-//
-// Delete url entries from the database
-//
-app.post('/urls/:id/delete', (req, res) => {
-  const idToDelete = req.params.id;
-  delete urlDatabase[idToDelete]; 
-  res.redirect('/urls');
-});
 
 //
 //Save Cookies
@@ -83,25 +88,16 @@ app.post('/urls/:id/delete', (req, res) => {
 app.post('/login', (req, res) => {
   const username = req.body.username
   res.cookie('username', username)
-  res.redirect('/urls')
-})
+  res.redirect('/urls');
+});
 
+//
+//Handle logout request 
+//
 app.post('/logout', (req, res) => {
-  const username = req.body.username
-  res.cookie('username', username)
-  res.redirect('/urls')
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
-
-// new route handler for /urls
-app.get('/urls', (req, res) => {
-  const templateVars = {
-    username: req.cookies['username'],
-    urls: urlDatabase 
-  };
-  //passes the URL data to our template
-  res.render("urls_index", templateVars);
-});
-
 
 //new route for /urls/new  - the form
 app.get('/urls/new', (req, res) => {
@@ -111,17 +107,29 @@ app.get('/urls/new', (req, res) => {
   res.render("urls_new.ejs", templateVars)
 });
 
-//new route for URL tinyIDs
-app.get('/urls/:id', (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]}
-  res.render('urls_show.ejs', templateVars)
+// new route handler for /urls - Added USERNAME and cookies
+app.get("/urls", (req, res) => {
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_index", templateVars);
 });
 
+// redirect any request to ("u/:id") to its longURL
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello!');
+//new route for URL tinyIDs
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  }
+  res.render('urls_show.ejs', templateVars)
 });
 
 // adds JSON string that reprents the entire urlDatabase objects at time of request
@@ -129,21 +137,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// displays HTML content that the /hello path responds with
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
-// set path created to display scope
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- // fetch path created to show error that you can't access value of a because of reference/scope error
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
 
  //
  // Listen
