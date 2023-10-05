6793f42e3e44efbd7de41b47f5feb73565fc9a7c
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-05 16:57:15 -0700

    completed helpers test for 3 exported helpers functions

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -15,11 +15,48 @@ const testUsers = {
   }
 };
 
+const testUrlDatabse = {
+  abc123: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: 'userRandomID'
+  },
+  def456: {
+    longURL: "http://www.google.com",
+    userID: 'user2RandomID'
+  }
+};
+
+
 describe('#getUserByEmail', () => {
   it('should return a user with valid email', () => {
     const user = getUserByEmail(testUsers, "user@example.com")
     const expectedUserID = "userRandomID";
-    console.log(user)
     assert.strictEqual(user, expectedUserID )
+  });
+});
+
+
+describe('#generateRandomString', () => {
+  it('should generate a random string at a specified input length', () => {
+    const sixCharacterString = generateRandomString(6);
+    assert.strictEqual(sixCharacterString.length, 6 )
+  });
+  it('should generate random strings that do not match', () => {
+    const string1 = generateRandomString(4);
+    const string2 = generateRandomString(4);
+    assert.notStrictEqual(string1, string2);
+  });
+});
+
+
+describe('#getUrlsForUser', () => {
+  it('should return all the url objects for a user', () => {
+    const urls = getUrlsForUser(testUrlDatabse, "userRandomID")
+    const expectedUrls = { 
+      abc123: {
+      longURL: "http://www.lighthouselabs.ca",
+      userID: 'userRandomID'
+    }}
+    assert.deepEqual(urls, expectedUrls )
   });
 });
