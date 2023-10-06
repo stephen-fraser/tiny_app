ed226356bb8c4e80e41406f113c30660a334cc6c
stephen-fraser <steve.james.fraser@gmail.com>
2023-10-05 17:13:11 -0700

    linted helpersTest

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,13 +4,13 @@ const {generateRandomString, getUrlsForUser, getUserByEmail} = require('../helpe
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
@@ -28,16 +28,16 @@ const testUrlDatabse = {
 
 
 describe('#getUserByEmail', () => {
-  
+
   it('should return a user with valid email', () => {
-    const user = getUserByEmail(testUsers, "user@example.com")
+    const user = getUserByEmail(testUsers, "user@example.com");
     const expectedUserID = "userRandomID";
-    assert.strictEqual(user, expectedUserID )
+    assert.strictEqual(user, expectedUserID);
   });
 
   it('should return undefined for email that is not in database', () => {
-    const user = getUserByEmail(testUsers, "hello@example.com")
-    assert.strictEqual(user, undefined )
+    const user = getUserByEmail(testUsers, "hello@example.com");
+    assert.strictEqual(user, undefined);
   });
 });
 
@@ -46,7 +46,7 @@ describe('#generateRandomString', () => {
 
   it('should generate a random string at a specified input length', () => {
     const sixCharacterString = generateRandomString(6);
-    assert.strictEqual(sixCharacterString.length, 6 )
+    assert.strictEqual(sixCharacterString.length, 6);
   });
 
   it('should generate random strings that do not match', () => {
@@ -60,17 +60,17 @@ describe('#generateRandomString', () => {
 describe('#getUrlsForUser', () => {
 
   it('should return all the url objects for a user', () => {
-    const urls = getUrlsForUser(testUrlDatabse, "userRandomID")
-    const expectedUrls = { 
+    const urls = getUrlsForUser(testUrlDatabse, "userRandomID");
+    const expectedUrls = {
       abc123: {
-      longURL: "http://www.lighthouselabs.ca",
-      userID: 'userRandomID'
-    }}
-    assert.deepEqual(urls, expectedUrls )
+        longURL: "http://www.lighthouselabs.ca",
+        userID: 'userRandomID'
+      }};
+    assert.deepEqual(urls, expectedUrls);
   });
 
   it('should return an empty obejct if the user has no urls to their name', () => {
-    const urls = getUrlsForUser(testUrlDatabse, "userGuyRandomID")
-    assert.deepEqual(urls, {})
+    const urls = getUrlsForUser(testUrlDatabse, "userGuyRandomID");
+    assert.deepEqual(urls, {});
   });
 });
